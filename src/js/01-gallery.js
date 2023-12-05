84af2db18b4c8593477bd13cd45fcef8a5e3c9ff
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-06 01:18:24 +0200

    1 and 2 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = galleryItems.map((item)=>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`
+).join('')
+  
+  galleryList.innerHTML = markup
+  
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSlide: 100,
+  });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const player = new Player(iframe);
+
+const onPlay = function(data){
+    localStorage.setItem(CURRENT_TIME_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+player
+ .setCurrentTime(currentTime)
+ .then(function (seconds){
+
+ })
+.catch(function(error){
+    switch (error.name){
+        case 'RangeError':
+        break;
+
+        default:
+
+        break;
+    }
+})
+
+
+ 
