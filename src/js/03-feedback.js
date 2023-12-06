5b183c1c3592f78e4f50624734df40308ce0cc6d
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-06 17:14:20 +0200

    three task completed
    wait check

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from 'lodash.throttle';
+import storage from './localStorage';
 
+// localStorage
+const STORAGE_KEY = 'feedback-form-state';
+
+// object form
+const refs = {
+  form: document.querySelector('form'),
+  eMail: document.querySelector('input'),
+  message: document.querySelector('textarea'),
+};
+
+console.log(refs);
+
+// set event listener
+refs.form.addEventListener('input', throttle(onIput, 500));
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+// clean form when submit information
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(refs.eMail.value, refs.message.value);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onIput(event) {
+  const { name, value } = event.target;
+  const savedFormData = storage.load(STORAGE_KEY) || {};
+  savedFormData[name] = value;
+  storage.save(STORAGE_KEY, savedFormData);
+}
+
+// get not saved information
+
+function outPut() {
+  const savedFormData = storage.load(STORAGE_KEY);
+
+  if (savedFormData) {
+    refs.eMail.value = savedFormData.email || '';
+    refs.message.value = savedFormData.message || '';
+  }
+}
+
+outPut();
